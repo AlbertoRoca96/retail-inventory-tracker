819b472fcda80c63f5b08d30e2852e62c94e8b94
AlbertoRoca-web <alroca308@gmail.com>
2025-08-19 05:50:49 -0400

    Create login.tsx

---
+++ apps/mobile/app/login.tsx
@@ -0,0 +1,7 @@
+// apps/mobile/app/login.tsx
+import React from 'react';
+import LoginScreen from '../src/screens/LoginScreen';
+
+export default function LoginRoute() {
+  return <LoginScreen />;
+}
